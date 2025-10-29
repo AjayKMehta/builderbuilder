3be403daffd0f7a811c5c14798affd5002e4f247
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-10-28 21:03:23 -0700

    Improve ModuleInitializer.

--- tests/BuilderGenerator.Tests/ModuleInitializer.cs
+++ tests/BuilderGenerator.Tests/ModuleInitializer.cs
@@ -19,6 +19,28 @@ internal static class ModuleInitializer
         if (assemblyConfigurationAttribute is not null)
             Console.WriteLine($"Build Configuration is {assemblyConfigurationAttribute.Configuration}.");
 
+        DiffTools.UseOrder(DiffTool.WinMerge);
+        ResolvedTool? _ = DiffTools.AddToolBasedOn(
+            DiffTool.WinMerge,
+            name: "MyDiffTool",
+            launchArguments: new(
+                Left: TargetLeftArguments,
+                Right: TargetRightArguments)
+            );
+
         VerifySourceGenerators.Initialize();
+    }
+    private static string TargetLeftArguments(string temp, string target)
+    {
+        var tempTitle = Path.GetFileName(temp);
+        var targetTitle = Path.GetFileName(target);
+        return $"/u /ignoreeol /wl /e \"{target}\" \"{temp}\" /dl \"{targetTitle}\" /dr \"{tempTitle}\" /cfg Backup/EnableFile=0";
+    }
+
+    private static string TargetRightArguments(string temp, string target)
+    {
+        var tempTitle = Path.GetFileName(temp);
+        var targetTitle = Path.GetFileName(target);
+        return $"/u /ignoreeol /wl /e \"{temp}\" \"{target}\" /dl \"{tempTitle}\" /dr \"{targetTitle}\" /cfg Backup/EnableFile=0";
     }
 }
